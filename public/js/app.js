815f1f425584b64b10fe75348263cd923e930f50
Divyansh Garg <divyanshgarg@Divyanshs-MacBook-Air.local>
2023-08-20 11:45:02 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   messageOne.textContent='Loading...'
   messageTwo.textContent=''
-  fetch(`http://localhost:3000/weather?address=${search.value}`).then(
+  fetch(`/weather?address=${search.value}`).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
